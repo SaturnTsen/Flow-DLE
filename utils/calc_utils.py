35452347db7e054a85f1371e8110adcfffc3eb6e
Yiming Chen <saturntsen@gmail.com>
2026-03-01 23:39:59 +0100

    update img and readme.md

--- utils/calc_utils.py
+++ utils/calc_utils.py
@@ -1,5 +1,5 @@
 # *************************************************************************
-# Copyright (2026) Yiming Chen, Linh Vu Tu
+# Copyright (2026) Yiming Chen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
